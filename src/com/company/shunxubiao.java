3b134ffee39b3a4f8f5d2ea8fded603018d0b372
xiangqin2002 <18671803827@163.com>
2019-05-01 11:20:03 +0800

    1

--- src/com/company/shunxubiao.java
+++ src/com/company/shunxubiao.java
@@ -3,8 +3,9 @@ package com.company;
 public class shunxubiao {
     public void paixu(int[] arr, int p){
         int n = arr.length;
-        for(int i = 0; i < p; i++){
-            replace(arr, i, p, n);
+        int p1 = p % n;
+        for(int i = 0; i < p1; i++){
+            replace(arr, i, p1, n);
             for(int j: arr)
                 System.out.print(j + " ");
             System.out.println();
@@ -13,9 +14,9 @@ public class shunxubiao {
             System.out.print(i + " ");
     }
 
-    private void replace(int[] arr, int i, int p, int n){
+    private void replace(int[] arr, int i, int p1, int n){
         int t = arr[i];
-        arr[i] = arr[n - p + i];
-        arr[n - p + i] = t;
+        arr[i] = arr[n - p1 + i];
+        arr[n - p1 + i] = t;
     }
 }
